8755678e9be83e44f4f9522b37382c9a8fd21be9
xxnithicxx <nguyenphucthinh106@gmail.com>
2022-04-24 23:31:59 +0700

    Make BAS Class
    Co-authored-by: Mackeddy <Mackeddy@users.noreply.github.com>
    Co-authored-by: phuhieu1505 <phuhieu1505@users.noreply.github.com>

--- Board.h
+++ Board.h
@@ -58,6 +58,96 @@ void ReleaseSudoku(int **board)
     delete[] board;
 }
 
+class BAS
+{
+private:
+    bool registered[9] = {false};
+
+public:
+    /**
+     * @brief Check if the number is valid in the row.
+     *
+     * @param board
+     * @param row
+     * @param number
+     * @return true if the number is already registered.
+     * @return false if the number is not yet registered.
+     */
+    bool isRegistered(int num)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (registered[i] == true && num == i + 1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Check row
+    void checkRow(int **board, int index)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (isRegistered(board[index][i]))
+            {
+                // Put BAS algorithm here.
+                /*
+                1.) The cell is not part of the current row or column being sorted
+                2.) The number in that cell has not been registered
+                3a.) The cell being swapped in OR out is not part of a row or column that has been previously sorted
+                3b.) The cell is in the row or column adjacent to the duplicate value. 
+                */
+
+                // Find position of the duplicate value.
+                // duplicatePos will be the one who we wish to swap fisrt
+                int duplicatePos = i;
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[index][j] == board[index][i])
+                    {
+                        duplicatePos = j;
+                        break;
+                    }
+                }
+                 
+                // 1.)
+                
+
+
+
+            }
+            else
+            {
+                registered[board[index][i] - 1] = true;
+            }
+        }
+    }
+
+    // Check column
+    void checkCollums(int **board, int index)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (isRegistered(board[i][index]))
+            {
+                // 1 7 9 2 0 / 3 4 5 6 8
+            }
+            else
+            {
+                registered[board[i][index] - 1] = true;
+            }
+        }
+    }
+}
+
+// Check and sort the board.
+void replenishSudoku(int **board)
+{
+    // Make a dump array that represent the element already registered.
+}
+
 void PrintSudoku(int **board)
 {
     for (int i = 0; i < 9; i++)
